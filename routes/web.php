44ef5fb386c35ac5c758152881b278afef285481
Waldecir Júnior <waldeciroajunior@hotmail.com>
2025-08-20 03:16:15 -0300

    feat: add transaction endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('transactions', TransactionController::class)->middleware('auth');
+Route::post('/create-transaction', [TransactionController::class, 'create'])->name('transaction.create');
