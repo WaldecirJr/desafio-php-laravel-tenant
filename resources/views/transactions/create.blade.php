5628f0fbedb90aa908fbb0ab1d51683267937d33
Waldecir Júnior <waldeciroajunior@hotmail.com>
2025-08-20 17:43:38 -0300

    style:  create view register-transaction and update create.blade

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -1,40 +1,39 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cadastro de Transações</title>
-</head>
-<body>
-    <h1>Cadastrar Transações</h1>
-    <form action="" method="POST">
-        @csrf
-        <div>
-            <label>Valor (R$):</label>
-            <input type="number" step="0.01" name="valor">
-        </div>
+@extends('layouts.register-transaction')
 
-        <div>
-            <label>CPF:</label>
-            <input type="text" name="cpf" maxlength="11">
-        </div>
+@section('content')
+    <div class="content">
+        <div class="main-container">
 
-        <div>
-            <label>Documento (PDF/JPG/PNG):</label>
-            <input type="file" name="documento">
-        </div>
+            <h1 class="page-title">Cadastrar Transações</h1>
+            <form action="{{ route('transactions.store') }}" method="POST" enctype="multipart/form-data"class="form-container">
+            @csrf
+        
+                <div class="mb-4">
+                    <label for="valor" class="form-label">Valor (R$):</label>
+                    <input type="number" step="0.01" name="valor" class="form-input">
+                </div>
 
-        <div>
-            <label>Status:</label>
-            <select name="status">
-                <option value="Em processamento">Em processamento</option>
-                <option value="Aprovada">Aprovada</option>
-                <option value="Negada">Negada</option>
-            </select>
-        </div>
+                <div class="mb-4">
+                    <label for="cpf" class="form-label">CPF:</label>
+                    <input type="text" name="cpf" maxlength="11" class="form-input">
+                </div>
 
-        <button type="submit">Salvar</button>
-    </form>
-</body>
-</html>
+                <div class="mb-4">
+                    <label for="documento" class="form-label">Documento (PDF/JPG/PNG):</label>
+                    <input type="file" name="documento" class="form-input">
+                </div>
+
+                <div class="mb-4">
+                    <label for="status" class="form-label">Status:</label>
+                    <select name="status" class="form-input">
+                        <option value="Em processamento">Em processamento</option>
+                        <option value="Aprovada">Aprovada</option>
+                        <option value="Negada">Negada</option>
+                    </select>
+                </div>
+
+                <button type="submit" class="btn-submit">Salvar</button>
+            </form>
+        </div>
+    </div>
+@endsection
